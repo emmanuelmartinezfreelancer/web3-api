3ec17ea3bcd15fb3f32d4f6e2798e91a40642ced
Emmanuel Martínez <121602270+emmanuelmartinezfreelancer@users.noreply.github.com>
2023-07-03 06:31:40 -0600

    Update firebase.js

--- api-interface/src/firebase.js
+++ api-interface/src/firebase.js
@@ -8,7 +8,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGIN_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 
